d8752db6c86cfc399d817fa12165b152746b9b6e
Felipe C. Barbosa <cavalcantebarbosa@gmail.com>
2024-08-18 18:19:38 -0300

    Apresentação do projeto Sorteador

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-asa
+function sortear(){
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até o número: ${ate}`);
+}
