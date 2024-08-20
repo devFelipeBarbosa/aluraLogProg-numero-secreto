764142bffe17c196af82c76c0bb829a294a75e55
Felipe C. Barbosa <cavalcantebarbosa@gmail.com>
2024-08-20 16:58:05 -0300

    finalizacao

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ function sortear() {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        reiniciar();
+        return;
+        
+    }
+
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+        reiniciar();
+        return;
+    }
+
     let sorteados = [];
     let numero;
 
@@ -11,6 +24,7 @@ function sortear() {
 
         while (sorteados.includes(numero)) {
             numero = obterNumeroAleatorio(de, ate);
+            alert('Tentando obter número inédito');
         }
         sorteados.push(numero);
 
@@ -41,5 +55,5 @@ function reiniciar() {
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
     document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
-    alterarStatusBotao(); 
+    alterarStatusBotao();
 }
